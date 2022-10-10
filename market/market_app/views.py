c3ef466a193a186c8f2f8149a547e317d18502f0
Алексей Вахрушев <valexva@bk.ru>
2022-10-10 09:28:11 +0300

    На странице sale.html список карточек продуктов сделан динамическим.

--- market/market_app/views.py
+++ market/market_app/views.py
@@ -1,10 +1,12 @@
+import datetime
+
 from django.views import View
 from django.views.generic import TemplateView
 
 # Поскольку меню категорий присутствует на всех страницах сайта, то
 # вероятно, его лучше реализовать через контекст-процессор
 
-# Зашлушка для меню категорий товара
+# Заглушка для меню категорий товара
 categories = [
     {
         'name': 'Компьютеры',
@@ -40,6 +42,26 @@ categories = [
     },
 ]
 
+date1 = datetime.date(year=2022, month=1, day=1)
+date2 = datetime.date(year=2022, month=2, day=2)
+
+# Заглушка для списка товаров
+product_list = [
+    {
+        'link': '#',
+        'image': '/static/assets/img/content/home/card.jpg',
+        'image_alt': 'card.jpg',
+        'title': 'Corsair Carbide Series Arctic White Steel',
+        'category': 'Games / XBox',
+        'price': 100,
+        'price_old': 120,
+        'sale': '-60%',
+        'date': date1,
+        'date_to': date2,
+        'description': 'Lorem ipsum dolor sit amet consectetuer adipiscing elit sed diam nonummy nibh euismod tincid unt ut laoreet dolore'
+    }
+] * 20
+
 
 class HomeView(TemplateView):
     """Главная страница"""
@@ -56,78 +78,13 @@ class AboutView(TemplateView):
                      }
 
 
-# Заглушка для списка истории просмотров покупателя
-history_view_list = [
-    {
-        'link': '#',
-        'image': '/static/assets/img/content/home/card.jpg',
-        'image_alt': 'card.jpg',
-        'title': 'Corsair Carbide Series Arctic White Steel',
-        'category': 'Games / XBox',
-        'price': 100,
-        'price_old': 120,
-        'sale': '-60%'
-    },
-    {
-        'link': '#',
-        'image': '/static/assets/img/content/home/card.jpg',
-        'image_alt': 'card.jpg',
-        'title': 'Corsair Carbide Series Arctic White Steel',
-        'category': 'Games / XBox',
-        'price': 100,
-        'price_old': 120,
-        'sale': '-60%'
-    },
-    {
-        'link': '#',
-        'image': '/static/assets/img/content/home/card.jpg',
-        'image_alt': 'card.jpg',
-        'title': 'Corsair Carbide Series Arctic White Steel',
-        'category': 'Games / XBox',
-        'price': 100,
-        'price_old': 120,
-        'sale': '-60%'
-    },
-    {
-        'link': '#',
-        'image': '/static/assets/img/content/home/card.jpg',
-        'image_alt': 'card.jpg',
-        'title': 'Corsair Carbide Series Arctic White Steel',
-        'category': 'Games / XBox',
-        'price': 100,
-        'price_old': 120,
-        'sale': '-60%'
-    },
-    {
-        'link': '#',
-        'image': '/static/assets/img/content/home/card.jpg',
-        'image_alt': 'card.jpg',
-        'title': 'Corsair Carbide Series Arctic White Steel',
-        'category': 'Games / XBox',
-        'price': 100,
-        'price_old': 120,
-        'sale': '-60%'
-    },
-    {
-        'link': '#',
-        'image': '/static/assets/img/content/home/card.jpg',
-        'image_alt': 'card.jpg',
-        'title': 'Corsair Carbide Series Arctic White Steel',
-        'category': 'Games / XBox',
-        'price': 100,
-        'price_old': 120,
-        'sale': '-60%'
-    },
-]
-
-
 class AccountView(TemplateView):
     """Account"""
     template_name = 'account.html'
     extra_context = {'middle_title_left': 'Личный кабинет',
                      'middle_title_right': 'Личный кабинет',
                      'categories': categories,
-                     'history_view_list': history_view_list
+                     'history_view_list': product_list
                      }
 
 
@@ -143,89 +100,102 @@ class CartView(TemplateView):
 class CatalogView(TemplateView):
     """Catalog"""
     template_name = 'catalog.html'
-    extra_context = {'middle_title_left': 'Catalog Megano', 'middle_title_right': 'Catalog'}
+    extra_context = {'middle_title_left': 'Catalog Megano',
+                     'middle_title_right': 'Catalog'}
 
 
 class CompareView(TemplateView):
     """Compare"""
     template_name = 'compare.html'
-    extra_context = {'middle_title_left': 'Сравнение товаров', 'middle_title_right': 'Сравнение товаров'}
+    extra_context = {'middle_title_left': 'Сравнение товаров',
+                     'middle_title_right': 'Сравнение товаров'}
 
 
 class ContactsView(TemplateView):
     """Contacts"""
     template_name = 'contacts.html'
-    extra_context = {'middle_title_left': 'Contact Megano', 'middle_title_right': 'Contact'}
+    extra_context = {'middle_title_left': 'Contact Megano',
+                     'middle_title_right': 'Contact'}
 
 
 class HistoryOrderView(TemplateView):
     """History order"""
     template_name = 'historyorder.html'
-    extra_context = {'middle_title_left': 'История заказов', 'middle_title_right': 'История заказов'}
+    extra_context = {'middle_title_left': 'История заказов',
+                     'middle_title_right': 'История заказов'}
 
 
 class HistoryViewView(TemplateView):
     """History view"""
     template_name = 'historyview.html'
-    extra_context = {'middle_title_left': 'История просмотра', 'middle_title_right': 'История просмотра'}
+    extra_context = {'middle_title_left': 'История просмотра',
+                     'middle_title_right': 'История просмотра'}
 
 
 class OneOrderView(TemplateView):
     """One order"""
     template_name = 'oneorder.html'
-    extra_context = {'middle_title_left': 'Заказ №200', 'middle_title_right': 'Заказ №200'}
+    extra_context = {'middle_title_left': 'Заказ №200',
+                     'middle_title_right': 'Заказ №200'}
 
 
 class OrderView(TemplateView):
     """Order"""
     template_name = 'order.html'
-    extra_context = {'middle_title_left': 'Оформление заказа', 'middle_title_right': 'Оформление заказа'}
+    extra_context = {'middle_title_left': 'Оформление заказа',
+                     'middle_title_right': 'Оформление заказа'}
 
 
 class PaymentView(TemplateView):
     """Payment"""
     template_name = 'payment.html'
-    extra_context = {'middle_title_left': 'Оплата', 'middle_title_right': 'Оплата'}
+    extra_context = {'middle_title_left': 'Оплата',
+                     'middle_title_right': 'Оплата'}
 
 
 class PaymentSomeOneView(TemplateView):
     """Payment someone"""
     template_name = 'paymentsomeone.html'
-    extra_context = {'middle_title_left': 'Оплата', 'middle_title_right': 'Оплата'}
+    extra_context = {'middle_title_left': 'Оплата',
+                     'middle_title_right': 'Оплата'}
 
 
 class ProductView(TemplateView):
     """Product"""
     template_name = 'product.html'
-    extra_context = {'middle_title_left': 'Megano Product', 'middle_title_right': 'Product'}
+    extra_context = {'middle_title_left': 'Megano Product',
+                     'middle_title_right': 'Product'}
 
 
 class ProfileView(TemplateView):
     """Profile"""
     template_name = 'profile.html'
-    extra_context = {'middle_title_left': 'Профиль', 'middle_title_right': 'Профиль'}
+    extra_context = {'middle_title_left': 'Профиль',
+                     'middle_title_right': 'Профиль'}
 
 
 class ProfileAvatarView(TemplateView):
     """Profile avatar"""
     template_name = 'profileAvatar.html'
-    extra_context = {'middle_title_left': 'Профиль', 'middle_title_right': 'Профиль'}
+    extra_context = {'middle_title_left': 'Профиль',
+                     'middle_title_right': 'Профиль'}
 
 
 class ProgressPaymentView(TemplateView):
     """Progress payment"""
     template_name = 'progressPayment.html'
-    extra_context = {'middle_title_left': 'Ожидание оплаты', 'middle_title_right': 'Ожидание оплаты'}
+    extra_context = {'middle_title_left': 'Ожидание оплаты',
+                     'middle_title_right': 'Ожидание оплаты'}
 
 
 class SaleView(TemplateView):
     """Sale"""
     template_name = 'sale.html'
-    extra_context = {'middle_title_left': 'Megano Blog', 'middle_title_right': 'Blog'}
-
-
-# Заглушка для просмотра популярных товаров
-popular_products = history_view_list
+    extra_context = {'middle_title_left': 'Megano Blog',
+                     'middle_title_right': 'Blog',
+                     'categories': categories,
+                     'cards_blog': product_list,
+                     }
 
 
 class ShopView(TemplateView):
@@ -234,7 +204,7 @@ class ShopView(TemplateView):
     extra_context = {'middle_title_left': 'About Megano',
                      'middle_title_right': 'About Us',
                      'categories': categories,
-                     'popular_products': popular_products,
+                     'popular_products': product_list,
                      }
 
 
