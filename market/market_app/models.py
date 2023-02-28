2c19885868985170eb79d59b25838c494b7549b2
Кудрявцев Сергей <joker-1237@mail.ru>
2023-02-28 12:10:38 +0300

    Added discount

--- market/market_app/models.py
+++ market/market_app/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 from mptt.models import MPTTModel, TreeForeignKey
 from django.utils.translation import gettext_lazy as _
 
@@ -72,6 +73,20 @@ class Category(MPTTModel):
         return reverse('catalog')
 
 
+class Discount(models.Model):
+    """Модель скидки для товаров"""
+    discount = models.IntegerField()
+    start_date = models.DateTimeField(verbose_name=_('дата начала'), default=timezone.now, blank=True, null=True)
+    end_date = models.DateTimeField(verbose_name=_('Дата окончания'), blank=True, null=True)
+
+    def __str__(self):
+        return str(self.discount)
+
+    class Meta:
+        verbose_name = _('скидка')
+        verbose_name_plural = _('скидки')
+
+
 class Product(models.Model):
     """Модель товара"""
     name = models.CharField(max_length=255, db_index=True, verbose_name=_('название'))
@@ -129,15 +144,6 @@ class CharacteristicValue(models.Model):
         return f'{self.characteristic.characteristic_name} {self.product.name}'
 
 
-class Discount(models.Model):
-    """Модель скидки для товаров"""
-    discount = models.IntegerField()
-
-    class Meta:
-        verbose_name = 'скидка'
-        verbose_name_plural = 'скидки'
-
-
 class ProductImage(models.Model):
     """Модель фотографий к товарам"""
     product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name='images')
@@ -145,21 +151,12 @@ class ProductImage(models.Model):
     image_alt = models.CharField(max_length=100, default='Фото к товару', verbose_name=_('подсказка'))
 
 
-class ProductDiscount(models.Model):
-    """Промежуточная модель между моделями товара и скидки"""
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Товар')
-    discount = models.ForeignKey(Discount, on_delete=models.CASCADE, verbose_name='Скидка')
-
-    class Meta:
-        verbose_name = 'Товар-скидка'
-        verbose_name_plural = 'Товар-скидка'
-
-
 class SellerProduct(models.Model):
     """Промежуточная модель между моделями товара и продавца"""
     product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name=_('товар'), related_name='sellers')
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE, verbose_name=_('продавец'), related_name='sellers')
     qty = models.PositiveIntegerField(verbose_name=_('количество'))
+    discount = models.ForeignKey(Discount, on_delete=models.CASCADE, verbose_name=_('скидка'), blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('цена'))
 
     def __str__(self):
