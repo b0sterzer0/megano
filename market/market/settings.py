4178003d2390b5c07f95b0376628cea0e77b7c3a
baykov1995 <baykov1995@gmail.com>
2023-03-12 19:22:30 +0300

    добавлена i18n в settings_app, Product, Sellers, базовый шаблон

--- market/market/settings.py
+++ market/market/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'mptt',
     'market_app.apps.MarketAppConfig',
     'perms_app',
-    'app_settings',
+    'app_settings.apps.AppSettingsConfig',
     'compare_app',
     'app_login',
     'api_for_payment_app',
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -141,6 +142,13 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+LANGUAGES = [
+    ('ru', 'Русский'),
+    ('en', 'English'),
+]
+
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale'), ]
 
 
 # Static files (CSS, JavaScript, Images)
