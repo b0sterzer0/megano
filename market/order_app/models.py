39d1c318fc039f84eeac4d91d041196b4d3b8083
Alexandr <writer.rus@mail.ru>
2023-03-22 13:23:06 +0300

    Добавлена i18n

--- market/order_app/models.py
+++ market/order_app/models.py
@@ -1,15 +1,16 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class OrderModel(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='заказчик')
-    json_order_data = models.JSONField(verbose_name='данные заказа в формате JSON')
-    activity = models.BooleanField(default=True, verbose_name='статус активности')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_('заказчик'))
+    json_order_data = models.JSONField(verbose_name=_('данные заказа в формате JSON'))
+    activity = models.BooleanField(default=True, verbose_name=_('статус активности'))
 
     class Meta:
-        verbose_name = 'заказ'
-        verbose_name_plural = 'заказы'
+        verbose_name = _('заказ')
+        verbose_name_plural = _('заказы')
 
     def __str__(self):
         return f'{self.user} - {self.activity}'
