48182c1d22c4b286823f890e8a79a3c025949f48
Кудрявцев Сергей <joker-1237@mail.ru>
2023-03-26 13:35:27 +0300

    added str info functions to utils.py

--- market/market_app/utils.py
+++ market/market_app/utils.py
@@ -1,9 +1,8 @@
 import random
-
+from django.shortcuts import render
 from django.core.cache import cache
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator, Page
 from django.db.models import Min
-
 from app_cart.models import AnonimCart
 from app_login.models import Profile
 from app_settings.utils import get_setting_from_db
@@ -159,7 +158,7 @@ def get_price(product_obj):
     return price_after_discount.price
 
 
-def get_catalog_product():
+def get_catalog_product(catalog=None):
     """
     Функция формирует удобный список с информацией для товаров на основе class Product
     """
@@ -171,21 +170,39 @@ def get_catalog_product():
         for value in values:
             if value.characteristic.characteristic_name.lower() == 'год выпуска':
                 year = int(value.value)
-        products_list.append(
-            {
-                'id': product_obj.id,
-                'link': product_obj.slug,
-                'images': {'first': {'image': {'url': product_obj.images.first().image.url}}},
-                'image_alt': {'first': {'image_alt': product_obj.images.first().image_alt}},
-                'name': product_obj.name,
-                'category': product_obj.category,
-                'price': get_price(product_obj),
-                'description': product_obj.description,
-                'count_reviews': get_count_product_reviews(product_obj),
-                'year': year,
-                'rating': product_obj.productpurchases.num_purchases,
-            }
-        )
+        try:
+            products_list.append(
+                {
+                    'id': product_obj.id,
+                    'link': product_obj.slug,
+                    'images': {'first': {'image': {'url': product_obj.images.first().image.url}}},
+                    'image_alt': {'first': {'image_alt': product_obj.images.first().image_alt}},
+                    'name': product_obj.name,
+                    'category': product_obj.category,
+                    'price': get_price(product_obj),
+                    'description': product_obj.description,
+                    'count_reviews': get_count_product_reviews(product_obj),
+                    'year': year,
+                    'rating': product_obj.productpurchases.num_purchases,
+                }
+            )
+        except Exception:
+            if catalog:
+                products_list.append(
+                    {
+                        'id': product_obj.id,
+                        'link': product_obj.slug,
+                        'images': {'first': {'image': {'url': product_obj.images.first().image.url}}},
+                        'image_alt': {'first': {'image_alt': product_obj.images.first().image_alt}},
+                        'name': product_obj.name,
+                        'category': product_obj.category,
+                        'price': get_price(product_obj),
+                        'description': product_obj.description,
+                        'count_reviews': get_count_product_reviews(product_obj),
+                        'year': year,
+                        'rating': 0,
+                    }
+                )
     return products_list
 
 
@@ -200,47 +217,90 @@ def get_seller_products(queryset):
         for value in values:
             if value.characteristic.characteristic_name.lower() == 'год':
                 year = int(value.value)
-        if product.discount:
-            sale = product.discount
-            products_list.append(
-                {
-                    'id': product.product_id,
-                    'seller': product.seller.name,
-                    'seller_id': product.seller.id,
-                    'link': product.product.slug,
-                    'images': {'first': {'image': {'url': product.product.images.first().image.url}}},
-                    'image_alt': {'first': {'image_alt': product.product.images.first().image_alt}},
-                    'name': product.product.name,
-                    'category': product.product.category,
-                    'price': round(float(product.price) * (1 - sale.discount / 100), 2),
-                    'price_old': product.price,
-                    'sale': sale.discount,
-                    'date': sale.start_date,
-                    'date_to': sale.end_date,
-                    'description': product.product.description,
-                    'count_reviews': get_count_product_reviews(product.product),
-                    'year': year,
-                    'rating': product.product.productpurchases.num_purchases,
-                }
-            )
-        else:
-            products_list.append(
-                {
-                    'id': product.product_id,
-                    'seller': product.seller.name,
-                    'seller_id': product.seller.id,
-                    'link': product.product.slug,
-                    'images': {'first': {'image': {'url': product.product.images.first().image.url}}},
-                    'image_alt': {'first': {'image_alt': product.product.images.first().image_alt}},
-                    'name': product.product.name,
-                    'category': product.product.category,
-                    'price': product.price,
-                    'description': product.product.description,
-                    'count_reviews': get_count_product_reviews(product.product),
-                    'year': year,
-                    'rating': product.product.productpurchases.num_purchases,
-                }
-            )
+        try:
+            if product.discount:
+                sale = product.discount
+                products_list.append(
+                    {
+                        'id': product.product_id,
+                        'seller': product.seller.name,
+                        'seller_id': product.seller.id,
+                        'link': product.product.slug,
+                        'images': {'first': {'image': {'url': product.product.images.first().image.url}}},
+                        'image_alt': {'first': {'image_alt': product.product.images.first().image_alt}},
+                        'name': product.product.name,
+                        'category': product.product.category,
+                        'price': round(float(product.price) * (1 - sale.discount / 100), 2),
+                        'price_old': product.price,
+                        'sale': sale.discount,
+                        'date': sale.start_date,
+                        'date_to': sale.end_date,
+                        'description': product.product.description,
+                        'count_reviews': get_count_product_reviews(product.product),
+                        'year': year,
+                        'rating': product.product.productpurchases.num_purchases,
+                    }
+                )
+            else:
+                products_list.append(
+                    {
+                        'id': product.product_id,
+                        'seller': product.seller.name,
+                        'seller_id': product.seller.id,
+                        'link': product.product.slug,
+                        'images': {'first': {'image': {'url': product.product.images.first().image.url}}},
+                        'image_alt': {'first': {'image_alt': product.product.images.first().image_alt}},
+                        'name': product.product.name,
+                        'category': product.product.category,
+                        'price': product.price,
+                        'description': product.product.description,
+                        'count_reviews': get_count_product_reviews(product.product),
+                        'year': year,
+                        'rating': product.product.productpurchases.num_purchases,
+                    }
+                )
+        except Exception:
+            if product.discount:
+                sale = product.discount
+                products_list.append(
+                    {
+                        'id': product.product_id,
+                        'seller': product.seller.name,
+                        'seller_id': product.seller.id,
+                        'link': product.product.slug,
+                        'images': {'first': {'image': {'url': product.product.images.first().image.url}}},
+                        'image_alt': {'first': {'image_alt': product.product.images.first().image_alt}},
+                        'name': product.product.name,
+                        'category': product.product.category,
+                        'price': round(float(product.price) * (1 - sale.discount / 100), 2),
+                        'price_old': product.price,
+                        'sale': sale.discount,
+                        'date': sale.start_date,
+                        'date_to': sale.end_date,
+                        'description': product.product.description,
+                        'count_reviews': get_count_product_reviews(product.product),
+                        'year': year,
+                        'rating': 0,
+                    }
+                )
+            else:
+                products_list.append(
+                    {
+                        'id': product.product_id,
+                        'seller': product.seller.name,
+                        'seller_id': product.seller.id,
+                        'link': product.product.slug,
+                        'images': {'first': {'image': {'url': product.product.images.first().image.url}}},
+                        'image_alt': {'first': {'image_alt': product.product.images.first().image_alt}},
+                        'name': product.product.name,
+                        'category': product.product.category,
+                        'price': product.price,
+                        'description': product.product.description,
+                        'count_reviews': get_count_product_reviews(product.product),
+                        'year': year,
+                        'rating': 0,
+                    }
+                )
     return products_list
 
 
@@ -293,3 +353,68 @@ def sort_list(cards, sort_by):
                 cards.sort(key=lambda x: x[sort_by])
         return cards
     return cards
+
+
+def get_catalog_products(request):
+    """
+    :param request:
+    Функция возвращает:
+    - список товаров по поиску из любой страницы
+    - список если фильтрация без цены
+    - список если фильтрация
+    - список если есть цена, наличие товаров на складе, название товара
+    - сортирует товары по популярности (продажам), цене, отзывам новизне (год выпуска товара)
+    """
+    cards = []
+    price, title, stock, sort_by, page, category = request.GET.get('price'), request.GET.get('title'), \
+                                                   request.GET.get('stock'), request.GET.get(
+        'sort_by'), request.GET.get('page'), request.GET.get('category')
+    if not price and not title and not category:
+        catalog = True
+        cards = get_catalog_product(catalog)
+        sort_list(cards, sort_by)
+        cards = get_product_list_by_page(cards, page)
+        context = {
+            'cards': cards,
+            'sort_by': sort_by
+        }
+        return context
+    if not price:
+        if not category:
+            cards_obj = SellerProduct.objects.select_related('product').filter(product__name__contains=title)
+            add_url = f'title={title}'
+            get_min_cards(cards, cards_obj)
+            sort_list(cards, sort_by)
+            cards = get_product_list_by_page(cards, page)
+        else:
+            cards_obj = SellerProduct.objects.select_related('product').filter(product__category_id=category)
+            add_url = f'category={category}'
+            get_min_cards(cards, cards_obj)
+            sort_list(cards, sort_by)
+            cards = get_product_list_by_page(cards, page)
+        context = {'cards': cards, 'add_url': add_url, 'sort_by': sort_by}
+        return context
+    price_product = price.replace(';', ' ').split()
+    cards_obj = SellerProduct.objects.select_related('product').filter(product__name__contains=title)
+    if stock:
+        cards_obj = SellerProduct.objects.select_related('product').filter(product__name__contains=title).filter(
+            qty__gt=0)
+    cards_list = get_seller_products(cards_obj)
+    for card in cards_list:
+        if int(price_product[0]) <= card['price'] <= int(price_product[1]):
+            cards.append(card)
+    for card_1 in cards_list:
+        for card_2 in cards:
+            if card_1['name'] == card_2['name'] and card_1['price'] < card_2['price']:
+                cards.pop(cards.index(card_2))
+    add_url = f'price={price_product[0]}%3B{price_product[1]}&title={title}'
+    if category:
+        add_url = f'category={category}&price={price_product[0]}%3B{price_product[1]}&title={title}'
+    cards = get_product_list_by_page(cards, page)
+    sort_list(cards, sort_by)
+    context = {
+        'cards': cards,
+        'add_url': add_url,
+        'sort_by': sort_by
+    }
+    return context
